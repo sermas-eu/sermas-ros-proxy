8fe25d4b9fef853273c33f151c0df0e82900638e
Marco Piazzola <marco.piazzola@spindox.it>
2024-07-09 16:04:19 +0200

    added integrations folders

---
+++ ros_proxy/ros_proxy/integrations/integration_list.py
@@ -0,0 +1,10 @@
+
+INTEGRATIONS_LIST = [BodyTracking,
+                     #  UserPosition(self, self.mqtt_client),
+                     #  RobotStatus(self, self.mqtt_client),
+                     #  RobotCmd(self, self.mqtt_client),
+                     #  RobotInitialPose(self, self.mqtt_client),
+                     #  RobotActuate(self, self.mqtt_client),
+                     #  RobotArmState(self, self.mqtt_client),
+                     #  RobotGripperState(self, self.mqtt_client)
+                     ]
---
+++ ros_proxy/ros_proxy/integrations/kinect/main.py
@@ -0,0 +1,65 @@
+import logging
+
+
+"""
+Forward user and intent detection to SERMAS toolkit
+"""
+
+
+class BodyTracking():
+  def __init__(self, ros_node):
+    # super().__init__(ros_node, mqtt_client, TopicDirection.ROS_TO_PLATFORM, ROS_USERS_LANDMARKS_TOPIC, SERMAS_USER_DETECTION_TOPIC)
+    self.ros_node = ros_node
+    self.mutual_haze = {}
+    self.last_ts = 0
+    self.ros_node.create_subscription(
+        MultipleUsersLandmarks, ROS_USERS_LANDMARKS_TOPIC, self.handle_ros_message, 10)
+    self.ros_node.create_subscription(
+        MutualGazeOutput, ROS_MUTUAL_GAZE_TOPIC, self.handle_ros_mutual_haze_message, 10)
+    logging.info("[MQTT] Subscribing to ROS topic %s" %
+                 ROS_USERS_LANDMARKS_TOPIC)
+
+  def handle_sermas_message(self, msg):
+    pass
+
+  def handle_ros_mutual_haze_message(self, msg):
+    if len(msg.body_ids) != len(msg.output):
+      logging.error('Mutual gaze output and body_ids len mismatch [%d != %d]' % (
+          len(msg.body_ids), len(msg.output)))
+      return
+    for index, id in enumerate(msg.body_ids):
+      self.mutual_haze[id] = msg.output[index]
+
+  def get_prob(self, body_id):
+    if body_id in self.mutual_haze:
+      return self.mutual_haze[body_id]
+    return 0
+
+  def handle_ros_message(self, msg):
+    detections = []
+    for u in msg.users:
+      if len(u.body_landmarks) < 4:
+        continue
+      l = u.body_landmarks[3]
+      logging.debug("Found NEK marker, distance: %.2f meters" % l.position.z)
+      detections.append({"user": {"value": u.body_id, "probability": self.get_prob(
+          u.body_id)}, "position": {"x": l.position.x, "y": l.position.y, "z": l.position.z}})
+    if len(detections) > 0:
+      '''
+      Publish user detection list
+      '''
+      logging.debug("Detected people: %d" % len(detections))
+      d = {"cameraId": "kinect", "source": "kinect", "detections": detections}
+      self.ros_node.mqtt_client.publish(self.sermas_topic, d)
+      '''
+      Publish intent detection list only if user is close enough
+      '''
+      for d in detections:
+        prob = d["user"]["probability"]
+        dist = d["position"]["z"]
+        if prob > INTENTION_PROBABILITY_THRESHOLD and dist < INTENTION_DISTANCE_THRESHOLD and self.last_ts < (time.time() - MIN_SENDING_INTERVAL_SEC):
+          self.last_ts = time.time()
+          d = {"moduleId": "detection", "source": "camera",
+               "probability": d["user"]["probability"], "interactionType": "start", "sessionId": ""}
+          self.ros_node.mqtt_client.publish(SERMAS_INTENT_DETECTION_TOPIC, d)
+          logging.info("Intent detection: %s" % str(d))
